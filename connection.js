992a38869a2305ecdc9d640f64111488da04aeb3
NouranMohsen <152489722+NouranMohsen@users.noreply.github.com>
2024-02-16 17:50:16 +0200

    Add files via upload

---
+++ connection.js
@@ -0,0 +1,79 @@
+const mysql      = require('mysql');
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'Nouran123',
+  database :'world'
+});
+
+
+
+connection.connect((err) => {
+  if (err) {
+    console.error('connection error' );
+    return;
+  }
+ 
+  console.log('connected ' );
+});
+
+
+connection.query('SELECT * FROM city', function (error, results, fields) {
+  if (error) throw error;
+  console.log( results);
+});
+
+//insert
+
+
+connection.query('INSERT INTO city SET ?', {
+  
+  Name: 'cairo',
+
+}
+, (err, result, fields) => {
+  if (err) {
+    console.log('Insertion error:');
+    throw err;
+  }
+  console.log(result);
+});
+
+//update 
+connection.query('update city  set ? where ?',[{Name :'cairo'},{CounrtyCode :'AFG'}],(err,result,fields) =>{
+  if (err) {
+    console.log(' error:');
+    throw err;
+  }
+  console.log(result);
+});
+
+
+//Delete 
+
+connection.query('delete from city where ?',{Name :'Dubai'},(err,result,fields) => {
+  if (err) {
+    console.log(' error on delete');
+    throw err;
+  }
+  console.log(result);
+
+});
+
+
+//create 
+
+const q=`CREATE TABLE IF NOT EXISTS x (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+)`;
+connection.query(q,(err,result ,fields) => {
+  if (err) {
+    console.log(' error');
+    throw err;
+  }
+  console.log(result);
+})
+
+connection.end();
+
